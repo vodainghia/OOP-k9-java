c07ae6a6587594fd54cf8dd1492b3208b2a4afd2
NghiaVo <nghiadaivo@gmail.com>
2023-09-08 22:09:10 +0700

    lab 01

---
+++ src/lab_01/lesson_01/VariableLearning.java
@@ -0,0 +1,15 @@
+package lab_01.lesson_01;
+
+public class VariableLearning {
+    public static void main(String[] args) {
+        byte myByteNumber = 1;
+        System.out.println(myByteNumber);
+
+        System.out.println(Byte.MAX_VALUE); //127
+        System.out.println(Short.MAX_VALUE); //32767
+
+        float myFloatNumber = 2.1F;
+        double myDoubleNumber = 2.11D;
+        long myLongNumber = 33535L;
+    }
+}
