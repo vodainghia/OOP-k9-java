ac51e9dd5fd32313ea255c3320ebb828011b7cb4
NghiaVo <nghiadaivo@gmail.com>
2023-12-02 19:55:51 +0700

    minor change in lab3_2

--- src/labs/lab_03/Lab_3_2.java
+++ src/labs/lab_03/Lab_3_2.java
@@ -2,12 +2,12 @@ package labs.lab_03;
 
 public class Lab_3_2 {
     public static void main(String[] args) {
-        int[] intArr = {1, 2, 3, 4, 5};
+        int[] intArr = {2, 1, 3, 5, 4};
         int min = intArr[0], max = intArr[0];
 
         for (int i : intArr) {
-            min = min > i ? i : min;
-            max = max < i ? i : max;
+            min = Math.min(min, i);
+            max = Math.max(max, i);
         }
 
         System.out.println("Min value of this integer array is: " + min);
